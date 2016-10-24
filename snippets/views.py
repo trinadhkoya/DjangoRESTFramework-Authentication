d190a296fe472a9b321156562d0427c83efc7f03
trinadhkoya <trinadhkoya9@gmail.com>
2016-10-24 23:30:00 +0530

    in this ,we made few changes like we removed mixins and updated with
     mixins.ListModelMixin,
                      mixins.CreateModelMixin,
                      generics.GenericAPIView)
    
     Now:we had
    
     generics.ListCreateAPIView
    
     mixins.RetrieveModelMixin,
                        mixins.UpdateModelMixin,
                        mixins.DestroyModelMixin,
                        generics.GenericAPIView
    
      generics.RetrieveUpdateDestroyAPIView

--- snippets/views.py
+++ snippets/views.py
@@ -1,41 +1,16 @@
 # Create your views here.
+from django.template.context_processors import csrf
 from rest_framework import generics
 from rest_framework import mixins
 
 from snippets.models import Snippets
 from snippets.serializers import SnippetSerializer
-
 '''
-
-in this everything in terms of class based views  like we weorked with functions
-
-'''
-
-
 class SnippetList(mixins.ListModelMixin,
                   mixins.CreateModelMixin,
                   generics.GenericAPIView):
     queryset = Snippets.objects.all();
     serializer_class = SnippetSerializer
-
-    '''
-         in this  waht we are doing is getting the data (snippets) and  creating the Snippets
- so we are using list and create mixins and methods as well
-
-    '''
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-'''
-# dealing with  update and Delete and Retrieve so
-# used corresponding mixins like Retrieve and update and Destroy and methods as well
-
-'''
 
 
 class SnippetDetail(mixins.RetrieveModelMixin,
@@ -43,18 +18,18 @@ class SnippetDetail(mixins.RetrieveModelMixin,
                     mixins.DestroyModelMixin,
                     generics.GenericAPIView):
     queryset = Snippets.objects.all();
-    # returns all snippets
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        print request
-        print args
-        print kwargs
+'''
 
-        return self.retrieve(request, *args, **kwargs)
+'''
+The minor change you can see from
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+'''
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippets.objects.all();
+    serializer_class = SnippetSerializer
 
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippets.objects.all()
+    serializer_class = SnippetSerializer
